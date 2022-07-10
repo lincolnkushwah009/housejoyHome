dd2da7ad4ba35ed4d9b6b7dca04374f6b4d96727
Lincoln Kushwah <lincolnkushwah009@gmail.com>
2022-07-10 16:30:07 +0530

    code

--- src/Components/home/index.js
+++ src/Components/home/index.js
@@ -9,6 +9,21 @@ import one from '../assets/1.png'
 import two from '../assets/2.png'
 import three from '../assets/3.png'
 import four from '../assets/4.png'
+import Hook from '../assets/hook.png'
+import Maintenance from '../assets/maintenance.png'
+import HomeAppliance from '../assets/home-appliance.png'
+import Bucket from '../assets/bucket.png'
+import Basket from '../assets/basket.png'
+import Relax from '../assets/relax.png'
+import pestControl from '../assets/pest-control.png'
+import renovation from '../assets/renovation.png'
+
+
+
+
+
+
+
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -42,8 +57,8 @@ class Index extends Component {
         });
     }
 
-    
-    
+
+
 
 
     render() {
@@ -59,7 +74,7 @@ class Index extends Component {
                     window.location.reload(false);
                     console.log(result.text);
                     console.log(result.text);
-                   
+
 
 
                 }, (error) => {
@@ -73,10 +88,135 @@ class Index extends Component {
         return (
 
 
-            <div>
+            <div >
+                <div class="cardBox">
+                <div class="cards">
+                        <div class="card-item">
+                            <div class="card-image">
+                            <img style={{ height: "150px", }} src={Hook} />
+                          
+                            </div>
+                            <div class="card-info">
+                                <h2 class="card-title">Construction</h2>
+                                <p class="card-intro">Project Management ConsultantWe provide end to end Construction services for Individual Residential Projects. From Ground Approvals, Documentation, Architectural Services, Material Vendors to Final Handover in a smooth and timely manner.</p>
+                                <button onClick={this.toggle} type="button" class="btn btn-primary">Enquiry</button>
+
+                            </div>
+                        </div>
 
 
-<Swiper
+                    </div>
+            
+                    <div class="cards">
+                        <div class="card-item">
+                            <div class="card-image">
+                            <img style={{ height: "150px", }} src={pestControl} />
+
+                            </div>
+                            <div class="card-info">
+                                <h2 class="card-title">ODS - Pest Control</h2>
+                                <p class="card-intro">Complete Pest Control Services to protect your Residential, Commercial or Organizational  space from pests, insects and Cockroaches.<br />Bed Bugs Control<br />Termite Control</p>
+                                <button onClick={this.toggle} type="button" class="btn btn-primary">Enquiry</button>
+
+                            </div>
+                        </div>
+
+
+                    </div>
+            
+                    <div class="cards">
+                        <div class="card-item">
+                            <div class="card-image">
+                            <img style={{ height: "150px", }} src={renovation} />
+                            </div>
+                            <div class="card-info">
+                                <h2 class="card-title">ODS - Home Repairs</h2>
+                                <p class="card-intro">Get your home repair services done by verified & trusted professionals. Background verified & skilled professionals. <br />Carpentry<br />Electrician<br />Plumbers </p>
+                                <button onClick={this.toggle} type="button" class="btn btn-primary">Enquiry</button>
+
+                            </div>
+                        </div>
+
+
+                    </div>
+               
+
+
+          
+                    <div class="cards">
+                        <div class="card-item">
+                            <div class="card-image">
+                            <img style={{ height: "150px", }} src={Relax} />
+                            </div>
+                            <div class="card-info">
+                                <h2 class="card-title">ODS - Grooming and Relaxation</h2>
+                                <p class="card-intro">Don't have enough time to visit a parlor? Relax at home and book trained professionals for waxing, threading, hair color, facial & clean up, & many more. <br />Salon Service for women<br />Salon Service for Men<br />Spa Services for Women<br />Spa Service for Men<br />Party Makeup  </p>
+                                <button onClick={this.toggle} type="button" class="btn btn-primary">Enquiry</button>
+
+                            </div>
+                        </div>
+
+
+                    </div>
+             
+                    <div class="cards">
+                        <div class="card-item">
+                            <div class="card-image">
+                            <img style={{ height: "150px", }} src={Basket} />
+                            </div>
+                            <div class="card-info">
+                                <h2 class="card-title">ODS - Cleaning Service</h2>
+                                <p class="card-intro">Complete cleaning services like deep cleaning, basic home cleaning by verified professionals at best price. <br />Bathroom Deep Cleaning<br />Kitchen Deep Cleaning<br />Sofa and Carpet Cleaning</p>
+                                <button onClick={this.toggle} type="button" class="btn btn-primary">Enquiry</button>
+
+                            </div>
+                        </div>
+
+
+                    </div>
+            
+                    <div class="cards">
+                        <div class="card-item">
+                            <div class="card-image">
+                            <img style={{ height: "150px", }} src={HomeAppliance} />
+                            </div>
+                            <div class="card-info">
+                                <h2 class="card-title">ODS - Appliances</h2>
+                                <p class="card-intro">Get your appliances repaired by our experts who are professionally trained & verified. Experts in Washing machines, AC Repair, Waterpurifier repair & many more. <br />Servicing, Repair, Installation & Uninstallation<br />Geyser<br />Refrigerator<br />Washing Machine</p>
+                                <button onClick={this.toggle} type="button" class="btn btn-primary">Enquiry</button>
+
+                            </div>
+                        </div>
+
+
+                    </div>
+     
+                    <div class="cards">
+                        <div class="card-item">
+                            <div class="card-image">
+                            <img style={{ height: "150px", }} src={Maintenance} />
+                            </div>
+                            <div class="card-info">
+                                <h2 class="card-title">ODS - Home Maintenance</h2>
+                                <p class="card-intro">Bored of your home interiors and wondering who will give it a new look? We are here to help you with verified & curated professionals.  <br />Painting
+                                    Interior <br />Renovate</p>
+                                <button onClick={this.toggle} type="button" class="btn btn-primary">Enquiry</button>
+
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+
+
+
+              
+
+
+
+
+                {/* <Swiper
                     // install Swiper modules
                     modules={[Navigation, Pagination, Scrollbar, A11y]}
                     spaceBetween={50}
@@ -87,146 +227,19 @@ class Index extends Component {
                     onSwiper={(swiper) => console.log(swiper)}
                     onSlideChange={() => console.log('slide change')}
                     style={{ padding: "40px" }}
-                >
-
-
-                  
-
-                    <SwiperSlide >
-                    <div class="cards">
-                                <div class="card-item">
-                                    <div class="card-image">
-                                    </div>
-                                    <div class="card-info">
-                                        <h2 class="card-title">Construction</h2>
-                                        <p class="card-intro">Project Management ConsultantWe provide end to end Construction services for Individual Residential Projects. From Ground Approvals, Documentation, Architectural Services, Material Vendors to Final Handover in a smooth and timely manner.</p>
-                                        <button onClick={this.toggle} type="button" class="btn btn-primary">Enquiry</button>
-
-                                    </div>
-                                </div>
-
-                               
-                            </div>
-                    </SwiperSlide>
+                > */}
 
 
 
-
-
-                    <SwiperSlide >
-                    <div class="cards">
-                                <div class="card-item">
-                                    <div class="card-image">
-                                    </div>
-                                    <div class="card-info">
-                                        <h2 class="card-title">ODS - Pest Control</h2>
-                                        <p class="card-intro">Complete Pest Control Services to protect your Residential, Commercial or Organizational  space from pests, insects and Cockroaches.<br/>Bed Bugs Control<br/>Termite Control</p>
-                                        <button onClick={this.toggle} type="button" class="btn btn-primary">Enquiry</button>
-
-                                    </div>
-                                </div>
-
-                               
-                            </div>
-                    </SwiperSlide>
-
-                    <SwiperSlide >
-                    <div class="cards">
-                                <div class="card-item">
-                                    <div class="card-image">
-                                    </div>
-                                    <div class="card-info">
-                                        <h2 class="card-title">ODS - Home Repairs</h2>
-                                        <p class="card-intro">Get your home repair services done by verified & trusted professionals. Background verified & skilled professionals. <br/>Carpentry<br/>Electrician<br/>Plumbers </p>
-                                        <button onClick={this.toggle} type="button" class="btn btn-primary">Enquiry</button>
-
-                                    </div>
-                                </div>
-
-                               
-                            </div>
-                    </SwiperSlide>
-
-
-                    <SwiperSlide >
-                    <div class="cards">
-                                <div class="card-item">
-                                    <div class="card-image">
-                                    </div>
-                                    <div class="card-info">
-                                        <h2 class="card-title">ODS - Grooming and Relaxation</h2>
-                                        <p class="card-intro">Don't have enough time to visit a parlor? Relax at home and book trained professionals for waxing, threading, hair color, facial & clean up, & many more. <br/>Salon Service for women<br/>Salon Service for Men<br/>Spa Services for Women<br/>Spa Service for Men<br/>Party Makeup  </p>
-                                        <button onClick={this.toggle} type="button" class="btn btn-primary">Enquiry</button>
-
-                                    </div>
-                                </div>
-
-                               
-                            </div>
-                    </SwiperSlide>
-                    
-
-
-
-                    <SwiperSlide >
-                    <div class="cards">
-                                <div class="card-item">
-                                    <div class="card-image">
-                                    </div>
-                                    <div class="card-info">
-                                        <h2 class="card-title">ODS - Cleaning Service</h2>
-                                        <p class="card-intro">Complete cleaning services like deep cleaning, basic home cleaning by verified professionals at best price. <br/>Bathroom Deep Cleaning<br/>Kitchen Deep Cleaning<br/>Sofa and Carpet Cleaning</p>
-                                        <button onClick={this.toggle} type="button" class="btn btn-primary">Enquiry</button>
-
-                                    </div>
-                                </div>
-
-                               
-                            </div>
-                    </SwiperSlide>
-
-
-
-                    <SwiperSlide >
-                    <div class="cards">
-                                <div class="card-item">
-                                    <div class="card-image">
-                                    </div>
-                                    <div class="card-info">
-                                        <h2 class="card-title">ODS - Appliances</h2>
-                                        <p class="card-intro">Get your appliances repaired by our experts who are professionally trained & verified. Experts in Washing machines, AC Repair, Waterpurifier repair & many more. <br/>Servicing, Repair, Installation & Uninstallation<br/>Geyser<br/>Refrigerator<br/>Washing Machine</p>
-                                        <button onClick={this.toggle} type="button" class="btn btn-primary">Enquiry</button>
-
-                                    </div>
-                                </div>
-
-                               
-                            </div>
-                    </SwiperSlide>
-
-
-                    <SwiperSlide >
-                    <div class="cards">
-                                <div class="card-item">
-                                    <div class="card-image">
-                                    </div>
-                                    <div class="card-info">
-                                        <h2 class="card-title">ODS - Home Maintenance</h2>
-                                        <p class="card-intro">Bored of your home interiors and wondering who will give it a new look? We are here to help you with verified & curated professionals.  <br/>Painting
-Interior <br/>Renovate</p>
-                                        <button onClick={this.toggle} type="button" class="btn btn-primary">Enquiry</button>
-
-                                    </div>
-                                </div>
-
-                               
-                            </div>
-                    </SwiperSlide>
-                   
-
-                </Swiper>
 
                
+                    
+            
+
+
+                {/* </Swiper> */}
+
+
 
 
                 <div className='row'>
@@ -239,17 +252,17 @@ Interior <br/>Renovate</p>
 
 
                 <div className='row'>
-                    <div className='col-sm-3' style={{marginBottom: "50px"}}><img style={{ height: "70px", }} src={one} />
-                    <p>Verified Professionals</p>
+                    <div className='col-sm-3' style={{ marginBottom: "50px" }}><img style={{ height: "70px", }} src={one} />
+                        <p>Verified Professionals</p>
                     </div>
-                    <div className='col-sm-3'style={{marginBottom: "50px"}}><img style={{ height: "70px", }} src={two} />
-                    <p>Insured Work</p>
+                    <div className='col-sm-3' style={{ marginBottom: "50px" }}><img style={{ height: "70px", }} src={two} />
+                        <p>Insured Work</p>
                     </div>
-                    <div className='col-sm-3' style={{marginBottom: "50px"}}><img style={{ height: "70px", }} src={three} />
-                    <p>Re-work Assurance</p>
+                    <div className='col-sm-3' style={{ marginBottom: "50px" }}><img style={{ height: "70px", }} src={three} />
+                        <p>Re-work Assurance</p>
                     </div>
-                    <div className='col-sm-3' style={{marginBottom: "50px"}}><img style={{ height: "70px", }} src={four} />
-                    <p>Professional Support</p>
+                    <div className='col-sm-3' style={{ marginBottom: "50px" }}><img style={{ height: "70px", }} src={four} />
+                        <p>Professional Support</p>
                     </div>
                 </div>
 
